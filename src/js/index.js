76b3eb3c4b29589195caa4f370032e8659af3f1d
REASON <reasonz@daum.net>
2023-06-04 20:53:51 +0900

    feat: genRandomColor 함수 추가

--- src/js/index.js
+++ src/js/index.js
@@ -18,9 +18,22 @@ const changeContentText = () => {
   });
 };
 
+export const genRandomColor = () => {
+  const candidate = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  const colorCode = [];
+
+  while (colorCode.length < 6) {
+    const pickIndex = Math.floor(Math.random() * candidate.length);
+    colorCode.push(candidate[pickIndex]);
+  }
+
+  return colorCode;
+};
+
 const init = () => {
   changeTitleText();
   changeContentText();
+  changeBgColor();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
