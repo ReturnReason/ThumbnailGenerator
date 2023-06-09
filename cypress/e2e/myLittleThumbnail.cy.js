8b63d8e9363e655524750630202bdd77303fa306
REASON <reasonz@daum.net>
2023-06-09 22:50:02 +0900

    test: genRandomColor 함수 리턴값 변경으로 인한 테스트 코드 수정

--- cypress/e2e/myLittleThumbnail.cy.js
+++ cypress/e2e/myLittleThumbnail.cy.js
@@ -37,10 +37,14 @@ describe('💙 썸네일 생성기 테스트', () => {
 
   it('✨ 랜덤한 배경 색상을 만드는 genRandomColor 함수는 16진수 6자리를 반환한다', () => {
     const colorCode = index.genRandomColor();
-    expect(colorCode).to.be.an('array').and.have.lengthOf(6);
+    expect(colorCode).to.be.lengthOf(6);
 
-    const isHexCode = colorCode.every((code) => /^[0-9a-fA-F]$/.test(code));
+    const isHexCode = [...colorCode].every((code) => /^[0-9a-fA-F]$/.test(code));
     expect(isHexCode).to.be.true;
+  });
+
+  it('✨ 랜덤 그라데이션 버튼을 누르면 2가지 색상을 가진 배경으로 적용된다.', () => {
+    cy.get('[data-test="btn_change-g-bg-color"]').click();
   });
 
   it('✨ 썸네일 생성 버튼을 누르면 썸네일이 png 확장자를 가진 이미지로 다운로드된다.', () => {
